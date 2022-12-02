5bfa8eacfff5405b6a590622d9401ba2fd095b8b
sadiq <mail@sadiq-pasha.com>
2022-12-02 11:56:00 -0800

    unsaved changes to day 2

--- day_2.py
+++ day_2.py
@@ -2,8 +2,9 @@ from day_2_input import day_2_input as raw_input_data
 
 # parse the input data
 raw_input_data = raw_input_data.split("\n")
-# raw_input_data = ["A Y","B X","C Z"]
-# dictionary of points for every possible round
+
+# dictionary of 
+# (points for every possible round, shapes to be played for part 2)
 case_dict = {"A X": (4,"Z"),
              "A Y": (8,"X"),
              "A Z": (3,"Y"),
@@ -15,11 +16,12 @@ case_dict = {"A X": (4,"Z"),
              "C Z": (6,"X")}
 
 # obtain each rounds score using the dictionary
-# and mapping each round using a lambda
+# for each round using a lambda
 # sum the array to find the score
 def score_calculator(input_data):
     return sum(map(lambda x: case_dict[x][0], input_data))
 
+# part one solution: 12740
 print(score_calculator(raw_input_data))
 
 # change the shapes played based on the new rules
@@ -27,6 +29,6 @@ def change_shapes_played(shapes_played):
     return shapes_played[0]+" "+case_dict[shapes_played][1]
 
 new_rules_data = list(map(change_shapes_played, raw_input_data))
+
+#part two solution: 11980
 print (score_calculator(new_rules_data))
-
-
